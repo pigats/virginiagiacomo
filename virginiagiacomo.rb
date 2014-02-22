5cdbde978c28bd6b9f10dce8054c617e02475a8f
pigats <andrea@fruktarbo.com>
2014-02-22 19:23:22 +0000

    add gifts new, create, show

--- virginiagiacomo.rb
+++ virginiagiacomo.rb
@@ -4,6 +4,7 @@ require 'haml'
 require 'coffee-script'
 require 'sinatra'
 require 'sinatra/asset_pipeline'
+require 'bson'
 
 require './goals'
 require './piggy_bank'
@@ -20,7 +21,7 @@ class VirginiaGiacomo < Sinatra::Base
 
 
   get '/' do
-
+    
     balance = settings.piggy_bank.balance
     goals = settings.goal.all
 
@@ -40,7 +41,28 @@ class VirginiaGiacomo < Sinatra::Base
       
     end
 
+    @goals_with_status.to_json
+
     haml :index
   end
+
+
+  # empty form
+  get '/gifts/new' do 
+    haml :'gifts/new'
+  end
+
+  # create gift
+  post '/gifts' do
+    id = settings.piggy_bank.deposit({name: params[:name], email: params[:email]}, params[:amount].to_i)
+    redirect '/gifts/' + id.to_s
+  end
+
+  # show gift
+  get '/gifts/:id' do 
+    @gift = settings.piggy_bank.find_deposit(BSON::ObjectId(params[:id]))
+    haml :'gifts/show'
+  end
+
 
 end
